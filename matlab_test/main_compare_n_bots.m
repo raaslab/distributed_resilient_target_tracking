060fc80ecb6ca3bfe79e72b470b0526850603e83
lovetuliper <lovetuliper@gmail.com>
2019-02-22 12:17:17 -0500

    add

--- matlab_test/main_compare_n_bots.m
+++ matlab_test/main_compare_n_bots.m
@@ -8,29 +8,28 @@
 clear all;
 global Nr N_tra Nt N_atk nei_range track_range
 
-Nt = 100;
+Nt = 50;
 
-N_trials = 10;
+N_trials = 30;
   
 % the # of robots, 
 min_num_bots = 10; 
 max_num_bots = 30; 
 
 % the # of worst-case attacks
-N_atk = 6; 
+N_atk = 5; 
 
 % the number of trajectories 
 N_tra = 4; 
 
 % neighbor range, within the distance, be carefully!!!
-nei_range = 120;
+nei_range = 30;
 
 % target tracking range
 track_range = 10; 
 
 % the range of robot position
 pos_range = 100;
-
 
 for Nr =  min_num_bots : max_num_bots
     robot_set_G = (1 : Nr);
@@ -39,19 +38,19 @@ for Nr =  min_num_bots : max_num_bots
         tar_pos = rand(Nt,2)*pos_range;
         
      % find all the non-overlapping maximal cliques
-    [nonovlap_cliqs_G, numofcliqs_G(i, Nr), cliqnum_G(i, Nr), com_cliq_form] = ...
+    [nonovlap_cliqs_G, n_cliqs_G(i, Nr), cliq_num_G(i, Nr), com_cliq_form] = ...
         nonoverlap_cliqs_fun(r_pos);   
     
     % obtain a set of targets tracked by each trajectory
     [tar_set_rtra, ~, n_tars_max_rtra, max_rtra_inx] = robot_tra_cover_fun(r_pos, tar_pos);
     
-%     % centralized resilient algorithm
-%     [n_cen_resi(i, Nr), rate_cen_resi(i, Nr), com_cen_resi(i, Nr), t_cen_resi(i, Nr)] = ...
-%         cen_resi_fun(robot_set_G, tar_set_rtra, n_tars_max_rtra, max_rtra_inx);
+    % centralized resilient algorithm
+    [n_cen_resi(i, Nr), rate_cen_resi(i, Nr), com_cen_resi(i, Nr), t_cen_resi(i, Nr)] = ...
+        cen_resi_fun(robot_set_G, tar_set_rtra, n_tars_max_rtra, max_rtra_inx);
     
-%     % centralized greedy algorithm
-%     [n_cen_gre(i, Nr), rate_cen_gre(i, Nr), com_cen_gre(i, Nr), t_cen_gre(i, Nr)] = ...
-%         cen_gre_fun(robot_set_G, tar_set_rtra); 
+    % centralized greedy algorithm
+    [n_cen_gre(i, Nr), rate_cen_gre(i, Nr), com_cen_gre(i, Nr), t_cen_gre(i, Nr)] = ...
+        cen_gre_fun(robot_set_G, tar_set_rtra); 
    
     % fully distribtued resilient
     [n_dis_resi(i, Nr), rate_dis_resi(i, Nr), com_dis_resi(i, Nr), t_dis_resi(i, Nr)] = ...
@@ -63,11 +62,11 @@ for Nr =  min_num_bots : max_num_bots
          com_dis_gre(i, Nr), t_dis_gre(i, Nr)] = ...
         fully_distri_gre_fun(tar_set_rtra, nonovlap_cliqs_G);
     
-    % fully distributed oblivious
-    [n_dis_obv(i, Nr), rate_dis_obv(i, Nr), ...
-        com_dis_obv(i, Nr), eva_dis_obv(i, Nr)]...
-        = fully_distri_obv_fun(tar_set_rtra, nonovlap_cliqs_G, ...
-        n_tars_max_rtra, max_rtra_inx);   
+%     % fully distributed oblivious
+%     [n_dis_obv(i, Nr), rate_dis_obv(i, Nr), ...
+%         com_dis_obv(i, Nr), eva_dis_obv(i, Nr)]...
+%         = fully_distri_obv_fun(tar_set_rtra, nonovlap_cliqs_G, ...
+%         n_tars_max_rtra, max_rtra_inx);   
     
 %     % hierarchcal central server with an oblivious algorithm
 %     [n_hier_cen_o(i, Nr), rate_hier_cen_o(i, Nr), ...
@@ -83,21 +82,22 @@ for Nr =  min_num_bots : max_num_bots
 end
 % plots 
   % compare the number of the remaining targets
-  figure; hold on; grid on;
-%   shadedErrorBar(min_num_bots : max_num_bots, n_cen_resi(:, min_num_bots : max_num_bots),...
-%       {@mean,@std}, 'lineprops', ':b','patchSaturation',0.33)
+  figure (1); hold on; grid on;
+ 
+  shadedErrorBar(min_num_bots : max_num_bots, n_cen_resi(:, min_num_bots : max_num_bots),...
+      {@mean,@std}, 'lineprops', ':b','patchSaturation',0.33)
   
-%   shadedErrorBar(min_num_bots : max_num_bots, n_cen_gre(:,min_num_bots : max_num_bots),...
-%       {@mean,@std}, 'lineprops', '-.k','patchSaturation',0.33)
+  shadedErrorBar(min_num_bots : max_num_bots, n_cen_gre(:,min_num_bots : max_num_bots),...
+      {@mean,@std}, 'lineprops', '-.k','patchSaturation',0.33)
   
   shadedErrorBar(min_num_bots : max_num_bots, n_dis_resi(:,min_num_bots : max_num_bots),...
       {@mean,@std},'lineprops','-r','patchSaturation',0.33)
   
   shadedErrorBar(min_num_bots : max_num_bots, n_dis_gre(:,min_num_bots : max_num_bots),...
-      {@mean,@std},'lineprops','-.k','patchSaturation',0.33)  
+      {@mean,@std},'lineprops','--g','patchSaturation',0.33)  
   
-  shadedErrorBar(min_num_bots : max_num_bots, n_dis_obv(:,min_num_bots : max_num_bots),...
-      {@mean,@std},'lineprops',':b','patchSaturation',0.33)   
+%   shadedErrorBar(min_num_bots : max_num_bots, n_dis_obv(:,min_num_bots : max_num_bots),...
+%       {@mean,@std},'lineprops',':b','patchSaturation',0.33)   
 
 %   shadedErrorBar(min_num_bots : max_num_bots, n_hier_cen_o(:,min_num_bots : max_num_bots),...
 %       {@mean,@std},'lineprops',':b','patchSaturation',0.33);
@@ -112,68 +112,67 @@ end
 %       'distributed greedy', 'distributed greedy, average');
 %     legend('', 'centralized resilient', '', 'centralized greedy', '', 'distributed resilient');
 %   legend('ful_dis', 'hier_cen_o', 'hier_cen_og', 'cen_resi', 'cen_gre');
-  legend('distributed resilient', 'distributed resilient average',...
-      'distributed greedy', 'distributed greedy average', ...
-      'distributed oblivious', 'distributed oblivious average');
+  legend('centralized resilient', 'centralized resilient average', ...
+      'centralized greedy', 'centralized greedy average',...
+      'distributed resilient', 'distributed resilient average',...
+      'distributed greedy', 'distributed greedy average');
   xlabel('number of robots','fontsize',11)
-  ylabel('coverage number','fontsize',11)
-         
+  ylabel('coverage number','fontsize',11)  
   
+% %   compare the worst-case attack rate
+%     figure; hold on;grid on;
   
-  % compare the worst-case attack rate
-  figure; hold on;grid on;
-  
-%   shadedErrorBar(min_num_bots : max_num_bots, rate_cen_resi(:, min_num_bots : max_num_bots),...
-%       {@mean,@std}, 'lineprops', ':b','patchSaturation',0.33)
- 
-%   shadedErrorBar(min_num_bots : max_num_bots, rate_cen_gre(:, min_num_bots : max_num_bots),...
-%       {@mean,@std}, 'lineprops', '-.k','patchSaturation',0.33) 
-  
-  shadedErrorBar(min_num_bots : max_num_bots, rate_dis_resi(:,min_num_bots : max_num_bots),...
-      {@mean,@std},'lineprops','-r', 'patchSaturation',0.33)
-  
-  shadedErrorBar(min_num_bots : max_num_bots, rate_dis_gre(:,min_num_bots : max_num_bots),...
-      {@mean,@std},'lineprops','-.k', 'patchSaturation',0.33)  
- 
-  shadedErrorBar(min_num_bots : max_num_bots, rate_dis_obv(:,min_num_bots : max_num_bots),...
-      {@mean,@std},'lineprops',':b', 'patchSaturation',0.33)   
-
-%   shadedErrorBar(min_num_bots : max_num_bots, rate_hier_cen_o(:,min_num_bots : max_num_bots),...
-%       {@mean,@std},'lineprops',':b','patchSaturation',0.33);
+% %   shadedErrorBar(min_num_bots : max_num_bots, rate_cen_resi(:, min_num_bots : max_num_bots),...
+% %       {@mean,@std}, 'lineprops', ':b','patchSaturation',0.33)
+%  
+% %   shadedErrorBar(min_num_bots : max_num_bots, rate_cen_gre(:, min_num_bots : max_num_bots),...
+% %       {@mean,@std}, 'lineprops', '-.k','patchSaturation',0.33) 
+%   
+%   shadedErrorBar(min_num_bots : max_num_bots, rate_dis_resi(:,min_num_bots : max_num_bots),...
+%       {@mean,@std},'lineprops','-r', 'patchSaturation',0.33)
+%   
+%   shadedErrorBar(min_num_bots : max_num_bots, rate_dis_gre(:,min_num_bots : max_num_bots),...
+%       {@mean,@std},'lineprops','-.k', 'patchSaturation',0.33)  
+%  
+%   shadedErrorBar(min_num_bots : max_num_bots, rate_dis_obv(:,min_num_bots : max_num_bots),...
+%       {@mean,@std},'lineprops',':b', 'patchSaturation',0.33)   
 % 
-%   shadedErrorBar(min_num_bots : max_num_bots, rate_hier_cen_og(:,min_num_bots : max_num_bots),...
-%       {@mean,@std}, 'lineprops', '-.k','patchSaturation',0.33)
-%g 
-  title('comparison of removal rate','fontsize',12)
-%   legend('centralized resilient', 'centralized resilient, average', ...
-%       'distributed resilient', 'distributed resilient, average',...
-%       'distributed greedy', 'distributed greedy, average');
-  legend('distributed resilient', 'distributed resilient average',...
-      'distributed greedy', 'distributed greedy average', ...
-      'distributed oblivious', 'distributed oblivious average');  
-%  legend('', 'centralized resilient', '', 'centralized greedy', '', 'distributed resilient');
-%   legend('ful_dis', 'hier_cen_o', 'hier_cen_og', 'cen_resi', 'cen_gre');
-  xlabel('number of targets','fontsize',11)
-  ylabel('worst-case attack rate','fontsize',11) 
+% %   shadedErrorBar(min_num_bots : max_num_bots, rate_hier_cen_o(:,min_num_bots : max_num_bots),...
+% %       {@mean,@std},'lineprops',':b','patchSaturation',0.33);
+% % 
+% %   shadedErrorBar(min_num_bots : max_num_bots, rate_hier_cen_og(:,min_num_bots : max_num_bots),...
+% %       {@mean,@std}, 'lineprops', '-.k','patchSaturation',0.33)
+% %g 
+%   title('comparison of removal rate','fontsize',12)
+% %   legend('centralized resilient', 'centralized resilient, average', ...
+% %       'distributed resilient', 'distributed resilient, average',...
+% %       'distributed greedy', 'distributed greedy, average');
+%   legend('distributed resilient', 'distributed resilient average',...
+%       'distributed greedy', 'distributed greedy average', ...
+%       'distributed oblivious', 'distributed oblivious average');  
+% %  legend('', 'centralized resilient', '', 'centralized greedy', '', 'distributed resilient');
+% %   legend('ful_dis', 'hier_cen_o', 'hier_cen_og', 'cen_resi', 'cen_gre');
+%   xlabel('number of targets','fontsize',11)
+%   ylabel('worst-case attack rate','fontsize',11) 
     
   
   % compare the communication 
-  figure; hold on;grid on;
+  figure (2); hold on;grid on;
   
-%   shadedErrorBar(min_num_bots : max_num_bots, com_cen_resi(:,min_num_bots : max_num_bots),...
-%       {@mean,@std}, 'lineprops', ':b','patchSaturation',0.33)
+  shadedErrorBar(min_num_bots : max_num_bots, com_cen_resi(:,min_num_bots : max_num_bots),...
+      {@mean,@std}, 'lineprops', ':b','patchSaturation',0.33)
  
-%   shadedErrorBar(min_num_bots : max_num_bots, com_cen_gre(:,min_num_bots : max_num_bots),...
-%       {@mean,@std}, 'lineprops', '-.k','patchSaturation',0.33)  
+  shadedErrorBar(min_num_bots : max_num_bots, com_cen_gre(:,min_num_bots : max_num_bots),...
+      {@mean,@std}, 'lineprops', '-.k','patchSaturation',0.33)  
 
   shadedErrorBar(min_num_bots : max_num_bots, com_dis_resi(:,min_num_bots : max_num_bots),...
       {@mean,@std},'lineprops','-r', 'patchSaturation',0.33)
 
   shadedErrorBar(min_num_bots : max_num_bots, com_dis_gre(:,min_num_bots : max_num_bots),...
-      {@mean,@std},'lineprops','-.k', 'patchSaturation',0.33)
+      {@mean,@std},'lineprops','--g', 'patchSaturation',0.33)
   
-  shadedErrorBar(min_num_bots : max_num_bots, com_dis_obv(:,min_num_bots : max_num_bots),...
-      {@mean,@std},'lineprops',':b', 'patchSaturation',0.33)  
+%   shadedErrorBar(min_num_bots : max_num_bots, com_dis_obv(:,min_num_bots : max_num_bots),...
+%       {@mean,@std},'lineprops',':b', 'patchSaturation',0.33)  
   
 %   shadedErrorBar(min_num_bots : max_num_bots, com_cen_o(:,min_num_bots : max_num_bots),...
 %       {@mean,@std},'lineprops',':b','patchSaturation',0.33);
@@ -183,34 +182,31 @@ end
 %g
 
   title('comparison of communication messages','fontsize',12)
-%   legend('centralized resilient', 'centralized resilient, average', ...
-%       'distributed resilient', 'distributed resilient, average',...
-%       'distributed greedy', 'distributed greedy, average');
-  legend('distributed resilient', 'distributed resilient average',...
-      'distributed greedy', 'distributed greedy average', ...
-      'distributed oblivious', 'distributed oblivious average');
-%  legend('', 'centralized resilient', '', 'centralized greedy', '', 'distributed resilient');
-%   legend('ful_dis', 'hier_cen_o', 'hier_cen_og', 'cen_resi', 'cen_gre');
+  legend('centralized resilient', 'centralized resilient average', ...
+      'centralized greedy', 'centralized greedy average',...
+      'distributed resilient', 'distributed resilient average',...
+      'distributed greedy', 'distributed greedy average');
   xlabel('number of robots','fontsize',11)
-  ylabel('communication messages','fontsize',11)   
+  ylabel('coverage number','fontsize',11) 
           
-  % compare the running time
-  figure; hold on;grid on;
   
-%   shadedErrorBar(min_num_bots : max_num_bots, t_cen_resi(:, min_num_bots : max_num_bots),...
-%       {@mean,@std}, 'lineprops', ':b','patchSaturation',0.33)
+  % compare the running time
+  figure (3); hold on;grid on;
+  
+  shadedErrorBar(min_num_bots : max_num_bots, t_cen_resi(:, min_num_bots : max_num_bots),...
+      {@mean,@std}, 'lineprops', ':b','patchSaturation',0.33)
  
-%   shadedErrorBar(min_num_bots : max_num_bots, t_cen_gre(:, min_num_bots : max_num_bots),...
-%       {@mean,@std}, 'lineprops', '-.k','patchSaturation',0.33)    
+  shadedErrorBar(min_num_bots : max_num_bots, t_cen_gre(:, min_num_bots : max_num_bots),...
+      {@mean,@std}, 'lineprops', '-.k','patchSaturation',0.33)    
 
   shadedErrorBar(min_num_bots : max_num_bots, t_dis_resi(:,min_num_bots : max_num_bots),...
       {@mean,@std},'lineprops','-r', 'patchSaturation',0.33)
   
   shadedErrorBar(min_num_bots : max_num_bots, t_dis_gre(:,min_num_bots : max_num_bots),...
-      {@mean,@std},'lineprops','-.k', 'patchSaturation',0.33) 
+      {@mean,@std},'lineprops','--g', 'patchSaturation',0.33) 
 
-  shadedErrorBar(min_num_bots : max_num_bots, eva_dis_obv(:,min_num_bots : max_num_bots),...
-      {@mean,@std},'lineprops',':b', 'patchSaturation',0.33)    
+%   shadedErrorBar(min_num_bots : max_num_bots, eva_dis_obv(:,min_num_bots : max_num_bots),...
+%       {@mean,@std},'lineprops',':b', 'patchSaturation',0.33)    
 
 %   shadedErrorBar(min_num_bots : max_num_bots, eva_cen_o(:,min_num_bots : max_num_bots),...
 %       {@mean,@std},'lineprops',':b','patchSaturation',0.33);
@@ -220,13 +216,20 @@ end
 %g
 
   title('comparison of computational time','fontsize',12)
-  legend('distributed resilient', 'distributed resilient average',...
-      'distributed greedy', 'distributed greedy average', ...
-      'distributed oblivious', 'distributed oblivious average');  
-%   legend('centralized resilient', 'centralized resilient, average', ...
-%       'distributed resilient', 'distributed resilient, average',...
-%       'distributed greedy', 'distributed greedy, average');
-%   legend('', 'centralized resilient', '', 'centralized greedy', '', 'distributed resilient');
-%   legend('ful_dis', 'hier_cen_o', 'hier_cen_og', 'cen_resi', 'cen_gre');
+  legend('centralized resilient', 'centralized resilient average', ...
+      'centralized greedy', 'centralized greedy average',...
+      'distributed resilient', 'distributed resilient average',...
+      'distributed greedy', 'distributed greedy average');
   xlabel('number of robots','fontsize',11)
-  ylabel('computational time','fontsize',11)   
+  ylabel('coverage number','fontsize',11)
+  
+  % plot the number of cliques
+  figure (4); hold on; grid on; 
+  shadedErrorBar(min_num_bots : max_num_bots, n_cliqs_G(:,min_num_bots : max_num_bots),...
+      {@mean,@std},'lineprops','-r', 'patchSaturation',0.33)
+  shadedErrorBar(min_num_bots : max_num_bots, cliq_num_G(:,min_num_bots : max_num_bots),...
+      {@mean,@std},'lineprops','--b', 'patchSaturation',0.33)   
+  title('number of cliqs and cliq number','fontsize',12)
+  legend('number of cliques', 'number of cliques average', 'clique number', 'clique number average');
+  xlabel('number of robots','fontsize',11)
+  ylabel('coverage number','fontsize',11)
